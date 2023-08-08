fcb367ed5194923e479f920877143078894bbefc
Aisalkyn <aisalkyn3541@gmail.com>
2023-08-08 15:02:18 +0600

    add

---
+++ src/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+
+
+                                                                                    
+    </div>
+  )
+}
+
+export default Header
